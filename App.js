7be3654993cae3b3e32004760e9ec27e1a7e8487
GenceErdagi <huseyingence.erdagi2018@pru.edu.tr>
2021-08-16 19:21:16 +0300

    Added states and onPress functionality

--- App.js
+++ App.js
@@ -1,29 +1,56 @@
-import React from "react";
-import { Button, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import {
+  Button,
+  Keyboard,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [text, setText] = useState("");
+  const [goals, setGoals] = useState([]);
   return (
-    <View style={{ paddingTop: 60, paddingHorizontal: 40 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-evenly",
-          alignItems: "center",
-        }}
-      >
+    <View style={styles.screen}>
+      <View style={styles.inputWrapper}>
         <TextInput
           placeholder={"Enter Course Goals!!"}
-          style={{
-            borderColor: "#121212",
-            borderWidth: 1,
-            borderRadius: 5,
-            paddingHorizontal: 20,
-            paddingVertical: 10,
+          style={styles.textInput}
+          onChangeText={(text) => setText(text)}
+          value={text}
+        />
+        <Button
+          title={"Add"}
+          onPress={() => {
+            setGoals([...goals, text]);
+            setText("");
+            Keyboard.dismiss();
           }}
         />
-        <Button title={"Add"} onPress={() => {}} />
       </View>
-      <View />
+      <View>
+        {goals.map((goal, i) => (
+          <Text key={i}>{goal}</Text>
+        ))}
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: { paddingTop: 60, paddingHorizontal: 40 },
+  inputWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textInput: {
+    borderColor: "#3b3b3b",
+    width: "80%",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+});
